1e7e4d26316ea11e9bc858ddb2ff0b8f1a906036
Vladimir Zoubritsky <vladimir.zoubritsky@authomize.com>
2022-08-09 18:00:19 +0300

    Use BaseSettings for configuration

--- onelogin_provider/configuration/client_configuration.py
+++ onelogin_provider/configuration/client_configuration.py
@@ -1,7 +1,9 @@
+from pydantic import Field
+
 from base_provider.configuration.base_client_configuration import BaseClientConfiguration
 
 
 class OneloginClientConfiguration(BaseClientConfiguration):
-    client_id: str
-    client_secret: str
+    client_id: str = Field(..., env="ONELOGIN_CLIENT_ID")
+    client_secret: str = Field(..., env="ONELOGIN_CLIENT_SECRET")
     region: str = 'us'
