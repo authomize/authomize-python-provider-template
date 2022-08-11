c75974d1dffb48265d87a8c81838b0f00fd32786
Maria Figovsky <maria.figovsky@authomize.com>
2022-08-12 02:59:29 +0300

    finishing

--- secret__server_provider/examples/flow.py
+++ secret__server_provider/examples/flow.py
@@ -7,7 +7,7 @@ from secret__server_provider.configuration.client_configuration import SecretSer
 from secret__server_provider.configuration.shared_configuration import SecretServerSharedConfiguration
 from secret__server_provider.workflows.health_checker import SecretServerHealthChecker
 from secret__server_provider.workflows.runner import SecretServerRunner
-
+from plugins.configuration import Configuration
 
 def example():
     basicConfig(
@@ -16,22 +16,22 @@ def example():
     )
 
     authomize_api_configuration = AuthomizeApiConfiguration(
-        #auth_token=getenv("AUTHOMIZE_API_TOKEN"),
-        #api_url=getenv("AUTHOMIZE_API_URL"),
-        auth_token='',
-        api_url='',
+        auth_token=getenv("AUTHOMIZE_API_TOKEN"),
+        api_url=getenv("AUTHOMIZE_API_URL"),
     )
     application_configuration = ApplicationConfiguration(
-        app_id=getenv("AUTHOMIZE_API_APP_ID"),
+        app_id='59971200755',
     )
-    client_configuration = SecretServerConfiguration(
-        '''
-        TODO : should not be hard coded token - fix later
-        client_id=getenv("SECRET_SERVER_CLIENT_ID"),
-        client_secret=getenv("SECRET_SERVER_CLIENT_SECRET"),
-        '''
-    )
+    
+    '''
+    TODO : should not be hard coded token - fix later
+    client_id=getenv("SECRET_SERVER_CLIENT_ID"),
+    client_secret=getenv("SECRET_SERVER_CLIENT_SECRET"),
+    '''
     shared_configuration = SecretServerSharedConfiguration()
+    client_configuration = SecretServerConfiguration(configuration=Configuration(host = "https://integrations.secretservercloud.com//api",
+                                                                                 api_key = {'BearerToken': ''}))
+
     health_checker = SecretServerHealthChecker(
         authomize_api_configuration=authomize_api_configuration,
         client_configuration=client_configuration,
@@ -42,7 +42,7 @@ def example():
         application_configuration=application_configuration,
         shared_configuration=shared_configuration,
     )
-    health_checker.raise_unhealthy()
+   # health_checker.raise_unhealthy()
     workflow_run.run()
 
 
