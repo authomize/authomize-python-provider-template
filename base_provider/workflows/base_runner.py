3e731f2dac8a6268488b7e9fad0aba32020d1a71
RoiAuthomize <90147027+RoiAuthomize@users.noreply.github.com>
2022-08-23 14:07:04 +0300

    Fix logger in the basic runner

--- base_provider/workflows/base_runner.py
+++ base_provider/workflows/base_runner.py
@@ -1,6 +1,4 @@
-import logging
-import sys
-from typing import Any, Iterable, List
+from typing import Iterable
 
 import structlog
 from authomize.rest_api_client.generated.schemas import RequestsBundleSchema
@@ -28,10 +26,14 @@ class BaseProviderRunner:
         self.client_configuration = client_configuration
         self.application_configuration = application_configuration
         self.shared_configuration = shared_configuration
-        self._init_logger()
+        self.logger = logger.bind(loader_name=self.loader_name)
+
+    @property
+    def loader_name(self):
+        return type(self).__name__
 
     def run(self):
-        logger.info("Starting provider")
+        self.logger.info("Starting provider")
         loader = BasicLoader(
             authomize_api_configuration=self.authomize_api_configuration,
             application_configuration=self.application_configuration,
@@ -39,72 +41,7 @@ class BaseProviderRunner:
         )
         transformed_data = self.get_transformed_data()
         loader(transformed_data)
-        logger.info("Provider done")
+        self.logger.info("Provider done")
 
     def get_transformed_data(self) -> Iterable[RequestsBundleSchema]:
         pass
-
-    def _init_logger(
-        self,
-        level=logging.INFO,
-        json: bool = False,
-        log_format=DEFAULT_FORMAT,
-    ):
-        timestamper = structlog.processors.TimeStamper(fmt="%Y-%m-%d %H:%M:%S")
-        shared_processors: List[Any] = [
-            structlog.stdlib.add_log_level,
-            structlog.stdlib.add_logger_name,
-            # Add extra attributes of LogRecord objects to the event dictionary
-            # so that values passed in the extra parameter of log methods pass
-            # through to log output.
-            structlog.stdlib.ExtraAdder(),
-            structlog.processors.format_exc_info,
-            timestamper,
-        ]
-
-        structlog.configure(
-            processors=shared_processors + [
-                # Prepare event dict for `ProcessorFormatter`.
-                structlog.stdlib.ProcessorFormatter.wrap_for_formatter,
-            ],
-            logger_factory=structlog.stdlib.LoggerFactory(),
-        )
-
-        renderer: Any = structlog.processors.JSONRenderer() if json else structlog.dev.ConsoleRenderer()
-        formatter = structlog.stdlib.ProcessorFormatter(
-            # These run ONLY on `logging` entries that do NOT originate within
-            # structlog.
-            foreign_pre_chain=shared_processors,
-            # These run on ALL entries after the pre_chain is done.
-            processors=[
-                # Remove _record & _from_structlog.
-                structlog.stdlib.ProcessorFormatter.remove_processors_meta,
-                renderer,
-            ],
-        )
-
-        handler = logging.StreamHandler()
-        # Use OUR `ProcessorFormatter` to format all `logging` entries.
-        handler.setFormatter(formatter)
-        root_logger = logging.getLogger()
-        root_logger.addHandler(handler)
-        root_logger.setLevel(level)
-
-        sys.excepthook = self.handle_exception
-        logging.captureWarnings(capture=True)
-        self.configure_common_loggers()
-
-    @staticmethod
-    def configure_common_loggers():
-        # charset_normalizer is noisy when using requests (directly and indirectly)
-        logging.getLogger('charset_normalizer').setLevel('WARNING')
-
-    @staticmethod
-    def handle_exception(exc_type, exc_value, exc_traceback):
-        """Handle all exceptions"""
-        # Handle Ctrl-C as usual in terminal
-        if issubclass(exc_type, KeyboardInterrupt):
-            sys.__excepthook__(exc_type, exc_value, exc_traceback)
-            return
-
-        logger.critical("Uncaught exception", exc_info=(exc_type, exc_value, exc_traceback))
