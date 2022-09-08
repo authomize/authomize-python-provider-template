4d454f888980d6d3deea3632d5cc46c5862ca178
Maria Figovsky <maria.figovsky@authomize.com>
2022-09-08 13:32:17 +0300

    isort and flake8

--- mock_provider/examples/flow.py
+++ mock_provider/examples/flow.py
@@ -1,10 +1,9 @@
+from base_provider.configuration.application_configuration import ApplicationConfiguration
+from base_provider.configuration.authomize_api_configuration import AuthomizeApiConfiguration
 from onelogin_provider.configuration.client_configuration import OneloginClientConfiguration
 from onelogin_provider.configuration.shared_configuration import OneloginSharedConfiguration
 from onelogin_provider.workflows.health_checker import OneloginHealthChecker
 from onelogin_provider.workflows.runner import OneloginRunner
-
-from base_provider.configuration.application_configuration import ApplicationConfiguration
-from base_provider.configuration.authomize_api_configuration import AuthomizeApiConfiguration
 
 
 def example():
--- secret_server_provider/configuration/client_configuration.py
+++ secret_server_provider/configuration/client_configuration.py
@@ -1,5 +1,5 @@
-from pydantic import Field
 import requests
+from pydantic import Field
 
 from base_provider.configuration.base_client_configuration import BaseClientConfiguration
 
@@ -7,7 +7,7 @@ from base_provider.configuration.base_client_configuration import BaseClientConf
 class SecretServerConfiguration(BaseClientConfiguration):
     # TODO : add default like host: str = Field(..., env="SECRET_SERVER_HOST",
     #        default="https://integrations.secretservercloud.com/api")
- 
+
     api_host: str = Field(..., env="SECRET_SERVER_HOST")
     user_name: str = Field(..., env="SECRET_SERVER_USERNAME")
     password: str = Field(..., env="SECRET_SERVER_PASSWORD")
@@ -17,7 +17,10 @@ class SecretServerConfiguration(BaseClientConfiguration):
     def generate_authentication_token(self):
         if (self.api_key == ""):
             headers = {"Content-Type": "application/x-www-form-urlencoded"}
-            data = {"username": "AuthomizeUser","grant_type": "password","password":"Authomize123!"}
+            data = {"username": "AuthomizeUser",
+                    "grant_type": "password",
+                    "password": "Authomize123!",
+                    }
             url = f'{self.api_host}/oauth2/token'
             self.api_host = f'{self.api_host}/api'
             response = requests.post(url, headers=headers, data=data)
