7bc8d996b379249bc65c21a075b83c7c6f84ed44
Maria Figovsky <maria.figovsky@authomize.com>
2022-09-08 13:46:12 +0300

    key field names are optional in addition to predefined acces-key and username keywords

--- secret_server_provider/extractors/secrets_last_access_key_extractor.py
+++ secret_server_provider/extractors/secrets_last_access_key_extractor.py
@@ -1,3 +1,4 @@
+from csv import field_size_limit
 from typing import Iterable
 
 from secret_server_openapiclient.apis import SecretsApi
@@ -23,15 +24,20 @@ class SecretsLastAccessKeyExtractor(BaseExtractor):
             "itemFileSize": null
     }
     """
-
+    predefined_interesting_keys = ["access-key", "username"]
     def extract_raw(self) -> Iterable[tuple[str, dict, str]]:
         data_provider_client: SecretServerClient = self.data_provider_client
         api_instance = SecretsApi(data_provider_client.openapi_client)
         all_secrets = get_paginated_results(api_instance.secrets_service_search_v2)
         for secret in all_secrets:
             normalized_secret_id = normalize_id(secret.id)
+            all_input_keys = []
             for field_key in data_provider_client.client_configuration.keys_to_fetch.split(','):
+                all_input_keys.append(field_key)
                 yield from self.get_records_by_slug(normalized_secret_id, field_key)
+            for field_key in self.predefined_interesting_keys:
+                if field_key not in all_input_keys:
+                    yield from self.get_records_by_slug(normalized_secret_id, field_key)
 
     def get_records_by_slug(self, secret_id: str, slug: str) -> Iterable[tuple[str, dict, str]]:
         key_history = self.get_secret_access_key_history(secret_id, slug)
