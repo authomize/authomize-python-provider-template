0f43b2316567db076f609e2cd870b42feac9d1de
Maria Figovsky <maria.figovsky@authomize.com>
2022-09-08 14:42:00 +0300

    working before optional fields

--- secret_server_provider/clients/secret_server_client.py
+++ secret_server_provider/clients/secret_server_client.py
@@ -1,5 +1,5 @@
 from secret_server_openapiclient import ApiClient, Configuration
-
+import requests
 from base_provider.clients.base_client import BaseClient
 from secret_server_provider.clients.simple_http_client import SimpleHttpClient
 from secret_server_provider.configuration.client_configuration import SecretServerConfiguration
@@ -11,10 +11,25 @@ class SecretServerClient(BaseClient):
         client_configuration: SecretServerConfiguration,
     ) -> None:
         super().__init__(client_configuration=client_configuration)
-        client_configuration.generate_authentication_token()
+        self.generate_authentication_token_and_update_configuration()
         configuration = Configuration(host=client_configuration.api_host)
         configuration.api_key['BearerToken'] = client_configuration.api_key
         # openapi client needs this
         self.configuration = configuration
         self.openapi_client = ApiClient(configuration=configuration)
         self.internal_api_client = SimpleHttpClient(client_configuration)
+
+    def generate_authentication_token_and_update_configuration(self):
+        if (self.client_configuration.api_key == ""):
+            headers = {"Content-Type": "application/x-www-form-urlencoded"}
+            data = {"username": "AuthomizeUser",
+                    "grant_type": "password",
+                    "password": "Authomize123!",
+                    }
+            url = f'{self.client_configuration.api_host}/oauth2/token'
+            response = requests.post(url, headers=headers, data=data)
+            response.raise_for_status()
+            bearer_token = response.json()['access_token']
+            # shared with http_simple_client for internal api and external api querying
+            self.client_configuration.set_new_host(f'{self.client_configuration.api_host}/api')
+            self.client_configuration.set_api_key(f'bearer {bearer_token}')
--- secret_server_provider/configuration/client_configuration.py
+++ secret_server_provider/configuration/client_configuration.py
@@ -1,13 +1,9 @@
-import requests
 from pydantic import Field
 
 from base_provider.configuration.base_client_configuration import BaseClientConfiguration
 
 
 class SecretServerConfiguration(BaseClientConfiguration):
-    # TODO : add default like host: str = Field(..., env="SECRET_SERVER_HOST",
-    #        default="https://integrations.secretservercloud.com/api")
-
     api_host: str = Field(..., env="SECRET_SERVER_HOST")
     user_name: str = Field(..., env="SECRET_SERVER_USERNAME")
     password: str = Field(..., env="SECRET_SERVER_PASSWORD")
@@ -16,16 +12,10 @@ class SecretServerConfiguration(BaseClientConfiguration):
     keys_to_fetch: str = Field(..., env="KEY_FIELD_NAMES")
     api_key: str = ""
 
-    def generate_authentication_token(self):
-        if (self.api_key == ""):
-            headers = {"Content-Type": "application/x-www-form-urlencoded"}
-            data = {"username": "AuthomizeUser",
-                    "grant_type": "password",
-                    "password": "Authomize123!",
-                    }
-            url = f'{self.api_host}/oauth2/token'
-            self.api_host = f'{self.api_host}/api'
-            response = requests.post(url, headers=headers, data=data)
-            response.raise_for_status()
-            bearer_token = response.json()['access_token']
-            self.api_key = f'bearer {bearer_token}'
+    # shared cofiguration api_key needs to be updated after token generation
+    # for a sake of the http_simple_client who uses token for internal api queries
+    def set_api_key (self, new_key):
+        self.api_key = new_key
+
+    def set_new_host(self, new_host):
+        self.api_host = new_host
